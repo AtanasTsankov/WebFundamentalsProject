3aa49eba674b8f4b8522fb7b9fd21177e4b1babe
Atanas Tsankov <atanas1914@gmail.com>
2017-03-20 18:56:11 +0200

    Fix on the carousel script. Still doesnt work every time

---
+++ js/carousel.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Atanas on 20-Mar-17.
+//  */
+var init = function() {
+    var carousel = document.getElementById('carousel'),
+        navButtons = document.querySelectorAll('#navigation button'),
+        panelCount = carousel.children.length,
+        transformProp = Modernizr.prefixed('transform'),
+        theta = 0,
+
+        onNavButtonClick = function( event ){
+            var increment = parseInt( event.target.getAttribute('data-increment') );
+            theta += ( 360 / panelCount ) * increment * -1;
+            carousel.style[ transformProp ] = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
+        };
+
+    for (var i=0; i < 2; i++) {
+        navButtons[i].addEventListener( 'click', onNavButtonClick, false);
+    }
+
+};
+
+window.addEventListener( 'DOMContentLoaded', init, false);
+
+// function rotateCarousel(button){
+//     var carousel = document.getElementById('carousel');
+//     panelCount = carousel.children.length;
+//     transformProp = Modernizr.prefixed('transform');
+//     theta = 0;
+//     var increment = parseInt( button.getAttribute('data-increment') );
+//     theta += ( 360 / panelCount ) * increment * -1;
+//     carousel.style[ transformProp ] = 'translateZ( -288px ) rotateY(' + 1 + 'deg)';
+// }
