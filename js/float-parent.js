0a361bbed0c42a25cd48d30a07e14fd9e3d0ab01
Atanas Tsankov <atanas1914@gmail.com>
2017-03-20 17:51:55 +0200

    Most of the content,styling and functionality

---
+++ js/float-parent.js
@@ -0,0 +1,99 @@
+/**
+ * Created by Atanas on 20-Mar-17.
+ */
+function floatParent1(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-1");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent2(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("question-2");
+    newDiv[0].style.left = "0";
+}
+
+function floatParent3(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("question-3");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent4(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("question-6");
+    newDiv[0].style.left = "0";
+}
+
+function floatParent5(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-2");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent6(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("question-4");
+    newDiv[0].style.left = "0";
+}
+
+function floatParent7(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("question-5");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent8(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-3");
+    newDiv[0].style.left = "0";
+}
+
+
+function floatParent9(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-4");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent10(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-5");
+    newDiv[0].style.left = "0";
+}
+
+function floatParent11(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-6");
+    newDiv[0].style.left = "0";
+}
+
+
+
+function floatParent12(button){
+    var parentDiv = button.parentNode.parentNode;
+    parentDiv.style.left = "200%";
+    var newDiv = document.getElementsByClassName("result-7");
+    newDiv[0].style.left = "0";
+}
